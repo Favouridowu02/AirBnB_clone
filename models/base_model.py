8d261ce1995b8e5f81ed27a9de89eb10e066c71d
Favour Idowu <favouridowu02@gmail.com>
2024-06-21 17:03:58 +0100

    made some changes to the bae model

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,27 @@ class BaseModel:
     """
         This class is the base model
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             This class sets up the public instance attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            date_format = '%Y-%m-%dT%H:%M:%S.%f'
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "updated_at":
+                        self.updated_at = datetime.strptime(kwargs[key], date_format)
+                    elif key == "created_at":
+                        self.created_at = datetime.strptime(kwargs[key], date_format)
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+        if not hasattr(self, 'id'):
+            self.id = str(uuid.uuid4())
 
     def __str__(self):
         """
@@ -45,3 +59,5 @@ class BaseModel:
         dic["created_at"] = self.created_at.isoformat(sep='T', timespec='auto')
 
         return dic
+
+
