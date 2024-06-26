c9ccc1e6da8bdd76a296dd3d08243628b3d68efa
Favour Idowu <favouridowu02@gmail.com>
2024-06-26 20:03:10 +0100

    Just added two classes

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    This Model contains the city class
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+        This is the city Model
+    """
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    This Module contains the State Class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+        This is the State Model
+    """
+    name = ""
