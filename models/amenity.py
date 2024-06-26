8e848b1386e47926ac7a1346cfa3fadbb0136cdc
Favour Idowu <favouridowu02@gmail.com>
2024-06-26 20:06:57 +0100

    Just added the amendity class

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    This Model contains the Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        This the Amenity CLass
+    """
+    name = ""
